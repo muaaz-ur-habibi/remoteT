43ed2cfc7ab7db98bf99d2178f8077f597b41d70
muaaz-ur-habibi <muaazarkhan@gmail.com>
2026-04-07 13:40:36 +0500

    implemented a password style login

--- src/client/client.go
+++ src/client/client.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
 )
 
+func checkUserAllowed(pwd string, conn net.Conn) bool {
+	_, err := conn.Write([]byte(pwd))
+	if err != nil {
+		fmt.Println("Error writing user to server")
+	}
+
+	resp := make([]byte, 1024)
+	n, err := conn.Read(resp)
+
+	if string(resp[:n]) == "allowed" {
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
 	addr := os.Args[1]
+	var pwd = ""
+	allowed := false
 
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
 		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Enter password:")
+	for i := 0; i < 3; i++ {
+		fmt.Scan(&pwd)
+
+		if checkUserAllowed(pwd, conn) {
+			allowed = true
+			break
+		}
+	}
+
+	if !allowed {
+		fmt.Println("Incorrect password")
+		conn.Close()
+		os.Exit(0)
 	}
 
 	go func() {
--- src/server/server.go
+++ src/server/server.go
@@ -8,6 +8,20 @@ import (
 	"os/exec"
 )
 
+func getServerPassword() string {
+	f, err := os.Open("server_pwd.txt")
+	if err != nil {
+		fmt.Println("Error opening server pwd file")
+	}
+
+	hash := make([]byte, 2048)
+	n, err := f.Read(hash)
+
+	hashstr := string(hash[:n])
+
+	return hashstr
+}
+
 func clientDC(conn net.Conn) {
 	conn.Close()
 	fmt.Println("Client " + conn.RemoteAddr().String() + " disconnected")
@@ -30,6 +44,24 @@ func handle(conn net.Conn) {
 	cmd.Wait()
 }
 
+func checkClientAllowed(conn net.Conn) {
+	pwd := make([]byte, 1024)
+
+	for i := 0; i < 3; i++ {
+		n, err := conn.Read(pwd)
+		if err != nil {
+			fmt.Println("Error reading pwd from client")
+		}
+
+		if string(pwd[:n]) == getServerPassword() {
+			conn.Write([]byte("allowed"))
+			break
+		} else {
+			conn.Write([]byte("not allowed"))
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Starting server")
 
@@ -46,6 +78,8 @@ func main() {
 		if err != nil {
 			log.Fatal(err.Error())
 		}
+
+		checkClientAllowed(conn)
 
 		fmt.Println("Connected with client " + conn.RemoteAddr().String())
 
